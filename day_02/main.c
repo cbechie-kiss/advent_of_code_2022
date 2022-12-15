6e173bd9bb1c2df1b3079db49a2ca3f62d609f77
carl-b-oshs <carl.bechie@kissprogramming.com>
2022-12-15 12:13:11 -0700

    day2 updated

--- day_02/main.c
+++ day_02/main.c
@@ -6,10 +6,17 @@
 #include "../advent_lib/error_handler.h"
 #include "../advent_lib/file_handler.h"
 
+
+#define FILE_NAME "input.txt"
+
 int main(){
  // (1 for Rock, 2 for Paper, and 3 for Scissors)
-
  // (0 if you lost, 3 if the round was a draw, and 6 if you won)
- 
+
+    FILE* fp = openFilePtr(FILE_NAME, "r");
+
+    
+
+
     return 0;
 }
